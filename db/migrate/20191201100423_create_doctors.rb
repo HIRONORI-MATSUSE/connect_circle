8501b4fbed659de33024aa4023c50e49a9604e4e
hiro <hironori.matsuse@gmail.com>
2019-12-01 19:06:40 +0900

    Add:doctorモデルの作成

---
+++ db/migrate/20191201100423_create_doctors.rb
@@ -0,0 +1,18 @@
+class CreateDoctors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :name_kana
+      t.integer :gender
+      t.date :birthday
+      t.integer :phone_number
+      t.text :comment
+      t.string :image
+      t.boolean :admin
+      t.references :user, foreign_key: true
+      t.references :clinic, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_095936) do
+ActiveRecord::Schema.define(version: 2019_12_01_100423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,23 @@ ActiveRecord::Schema.define(version: 2019_12_01_095936) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "name_kana"
+    t.integer "gender"
+    t.date "birthday"
+    t.integer "phone_number"
+    t.text "comment"
+    t.string "image"
+    t.boolean "admin"
+    t.bigint "user_id"
+    t.bigint "clinic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clinic_id"], name: "index_doctors_on_clinic_id"
+    t.index ["user_id"], name: "index_doctors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +53,6 @@ ActiveRecord::Schema.define(version: 2019_12_01_095936) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "doctors", "clinics"
+  add_foreign_key "doctors", "users"
 end
