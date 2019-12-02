a2f688580dda6ad56d4a1234b4dcaa246a451809
hiro <hironori.matsuse@gmail.com>
2019-12-02 13:15:57 +0900

    Add:seedデータの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Clinic.create!(
+  [
+    {
+      name: '松瀬鍼灸整骨院',
+      address: '渋谷区',
+      phone_number: '03-1234-5678',
+      image: 'string'
+    }
+  ]
+)
+
+User.create!(
+  [
+    {
+      email: 'test@gmail.com',
+      password: '123456',
+      admin: 'true'
+    }
+  ]
+)
+
+Doctor.create!(
+  [
+    {
+      name: '松瀬',
+      name_kana: 'まつせ',
+      gender: '1',
+      birthday: '2019-12-01',
+      phone_number: '090-1234-5678',
+      comment: 'aaaaa',
+      image: 'string',
+      admin: 'true'
+    }
+  ]
+)
