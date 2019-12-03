3f43a89d915dcb4a6ae25555ff1a23f7437d3ebd
hiro <hironori.matsuse@gmail.com>
2019-12-03 11:35:36 +0900

    Add:パラメータの設定と新規作成の日本語化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+    if current_user.try(:admin?)
+      @doctor = @user.build_doctor
+    else
+      @patient = @user.build_patient
+    end
+  end
 
   # POST /resource
   # def create
