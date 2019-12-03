9129d521b5ae59c31627c015e5fa543217f31752
hiro <hironori.matsuse@gmail.com>
2019-12-03 15:12:41 +0900

    Add:ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' } 
+    resources :doctors 
+    resources :patients
 
-  resources :doctors do
-     members :qualificate
-  end
-  resources :patients
+
+  # devise_for :users, :controllers => {:registrations => "registrations"} do
+  #   # get "communities/:community_id/sign_up" => "registrations#new", :as => :new_community_user_registration
+  #   get "users/:user_id/users/sign_up" => "registrations#new", :as => :new_user_registration
+  #   post "users/:user_id/users" => "registrations#create", :as => :user_registration
+  # end
+
+  
 end
