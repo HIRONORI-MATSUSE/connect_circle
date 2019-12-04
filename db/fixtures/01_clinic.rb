68937ac7a20ff5d9e63ebe4ad88e5df09f3cd7a1
hiro <hironori.matsuse@gmail.com>
2019-12-04 18:26:49 +0900

    Add:seedデータの作成

---
+++ db/fixtures/01_clinic.rb
@@ -0,0 +1,195 @@
+Clinic.seed do |s|
+s.id = 1
+s.name = "松瀬鍼灸整骨院"
+s.address = "渋谷区一丁目01番地"
+s.phone_number = "03-1014-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 2
+s.name = "松瀬2鍼灸整骨院"
+s.address = "渋谷区一丁目02番地"
+s.phone_number = "03-1024-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 3
+s.name = "松瀬3鍼灸整骨院"
+s.address = "渋谷区一丁目03番地"
+s.phone_number = "03-1034-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 4
+s.name = "松瀬4鍼灸整骨院"
+s.address = "渋谷区一丁目03番地"
+s.phone_number = "03-1044-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 5
+s.name = "松瀬5鍼灸整骨院"
+s.address = "渋谷区一丁目05番地"
+s.phone_number = "03-1054-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 6
+s.name = "松瀬6鍼灸整骨院"
+s.address = "渋谷区一丁目06番地"
+s.phone_number = "03-1064-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 7
+s.name = "松瀬7鍼灸整骨院"
+s.address = "渋谷区一丁目07番地"
+s.phone_number = "03-1074-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 8
+s.name = "松瀬8鍼灸整骨院"
+s.address = "渋谷区一丁目08番地"
+s.phone_number = "03-1084-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 9
+s.name = "松瀬9鍼灸整骨院"
+s.address = "渋谷区一丁目09番地"
+s.phone_number = "03-1094-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 10
+s.name = "松瀬10鍼灸整骨院"
+s.address = "渋谷区一丁目10番地"
+s.phone_number = "03-1104-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 11
+s.name = "松瀬11鍼灸整骨院"
+s.address = "渋谷区一丁目11番地"
+s.phone_number = "03-1114-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 12
+s.name = "松瀬12鍼灸整骨院"
+s.address = "渋谷区一丁目12番地"
+s.phone_number = "03-1124-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 13
+s.name = "松瀬13鍼灸整骨院"
+s.address = "渋谷区一丁目123番地"
+s.phone_number = "03-1134-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 14
+s.name = "松瀬14鍼灸整骨院"
+s.address = "渋谷区一丁目14番地"
+s.phone_number = "03-1144-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 15
+s.name = "松瀬15鍼灸整骨院"
+s.address = "渋谷区一丁目15番地"
+s.phone_number = "03-1154-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 16
+s.name = "松瀬16鍼灸整骨院"
+s.address = "渋谷区一丁目16番地"
+s.phone_number = "03-1164-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 17
+s.name = "松瀬17鍼灸整骨院"
+s.address = "渋谷区一丁目17番地"
+s.phone_number = "03-1174-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 18
+s.name = "松瀬18鍼灸整骨院"
+s.address = "渋谷区一丁目18番地"
+s.phone_number = "03-1184-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 19
+s.name = "松瀬19鍼灸整骨院"
+s.address = "渋谷区一丁目19番地"
+s.phone_number = "03-1194-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 20
+s.name = "松瀬20鍼灸整骨院"
+s.address = "渋谷区一丁目20番地"
+s.phone_number = "03-1204-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 21
+s.name = "松瀬21鍼灸整骨院"
+s.address = "渋谷区一丁目21番地"
+s.phone_number = "03-1214-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 22
+s.name = "松瀬22鍼灸整骨院"
+s.address = "渋谷区一丁目22番地"
+s.phone_number = "03-1224-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 23
+s.name = "松瀬23鍼灸整骨院"
+s.address = "渋谷区一丁目23番地"
+s.phone_number = "03-1234-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+Clinic.seed do |s|
+s.id = 24
+s.name = "松瀬24鍼灸整骨院"
+s.address = "渋谷区一丁目24番地"
+s.phone_number = "03-1244-5478"
+s.image = File.join(Rails.root, "db/fixtures/images/the-road-815297__480.jpg")
+end
+
+
+
+
---
+++ db/fixtures/04_patient.rb
@@ -0,0 +1,15 @@
+i = 1
+20.times {
+  gimei = Gimei.new
+  Patient.seed do |s|
+    s.user_id = User.find_by(id: "2#{i}".to_i)
+    s.name = gimei.name.kanji
+    s.name_kana = gimei.name.hiragana
+    s.gender = 1
+    s.birthday = "19#{i}-12-01"
+    s.address = gimei.address.prefecture.kanji
+    s.phone_number = "#{i}0-1234-5678"
+    s.image = File.join(Rails.root, "db/fixtures/images/f_f_object_174_s256_f_object_174_0bg.png")
+    i += 1
+  end
+}
