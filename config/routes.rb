c0d2a50fe107c3a1b8852c4f4faa9bd629c45f72
hiro <hironori.matsuse@gmail.com>
2019-12-04 10:29:51 +0900

    Add:ルーティングの再設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,11 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # devise_for :users, controllers: { registrations: 'users/registrations' } 
-  # resources :doctors 
-  # resources :patients
-  
-
-
-  devise_for :users, :controllers => {:registrations => "registrations"} do
-    # get "communities/:community_id/sign_up" => "registrations#new", :as => :new_community_user_registration
-    get "users/:user_id/users/sign_up" => "registrations#new", :as => :new_user_registration
-    post "users/:user_id/users" => "registrations#create", :as => :user_registration
+  namespace :user do
+    resources :doctor
+    resources :patient 
   end
 
 end
