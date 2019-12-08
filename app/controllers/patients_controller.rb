cfda0e8ae7c1399602cd3b7ac03b2811d047f31c
hiro <hironori.matsuse@gmail.com>
2019-12-08 16:35:53 +0900

    Add:マイページ編集画面の設定

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,19 +1,29 @@
 class PatientsController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update]
+
 
   def show
-     @patinet = Patient.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: '編集しました'
+    else
+      render 'edit'
+    end
   end
 
   private
 
+  def set_user
+    @patient = Patient.find(params[:id])
+  end
+
   def patient_params
-    params.require(:patient).permit(:name)
+    params.require(:patient).permit(:name, :name_kana, :gender, :birthday, :address, :phone_number, :image)
   end
 
 end
