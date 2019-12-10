78952a120036d48d16d7073eb6f4e647deb4eddf
hiro <hironori.matsuse@gmail.com>
2019-12-10 20:24:55 +0900

    Add:インフォメーション作成と治療院編集機能の実装

--- app/controllers/staff/informations_controller.rb
+++ app/controllers/staff/informations_controller.rb
@@ -5,12 +5,18 @@ class Staff::InformationsController < ApplicationController
   end
 
   def create
-    @information = current_user.clinics.new(task_params)
+    @information = current_user.doctor.clinic.new(information_params)
     if @information.save
-      redirect_to staff_clinic_path(current_user.doctor.clinic.id), notice: '作成しました'
+      redirect_to staff_clinic_path(current_user.doctor.clinic.id), notice: 'お知らせを作成しました'
     else
       render 'new'
     end
+  end
+
+  private
+
+  def information_params
+    params.require(:information).permit(:comment)
   end
 
 end
--- db/fixtures/01_clinic.rb
+++ db/fixtures/01_clinic.rb
@@ -1,32 +1,20 @@
+image1 = Rack::Test::UploadedFile.new(Rails.root.join("db/fixtures/images/the-road-815297__480.jpg"))
 
+threads = []
 i = 1
-20.times {
-  Clinic.seed do |s|
-    s.name = "松瀬#{i}鍼灸整骨院",
-    s.address = "渋谷区一丁目#{i}番地",
-    s.phone_number = "03-1244-5478",
-    s.image = Rack::Test::UploadedFile.new(Rails.root.join("db/fixtures/images/the-road-815297__480.jpg"))
-    i += 1
+20.times do
+  threads << Thread.new do
+    ActiveRecord::Base.connection_pool.with_connection do
+      Clinic.create(
+        name: "松瀬#{i}鍼灸整骨院",
+        address: "渋谷区一丁目#{i}番地",
+        phone_number: "03-1244-5478",
+        image: image1
+      )
+    end
   end
-}
-
-# threads = []
-# User.last(20).each do |user|
-#   threads << Thread.new do
-#     ActiveRecord::Base.connection_pool.with_connection do
-#       gimei = Gimei.new.name
-#       user.build_patient(
-#         name: gimei.kanji,
-#         name_kana: gimei.hiragana,
-#         gender: rand(0...2),
-#         birthday: Time.new,
-#         phone_number: 111111111,
-#         address: "渋谷区",
-#         image: image2
-#       ).save
-#     end
-#   end
-# end
-# threads.each(&:join)
+  i += 1
+end
+threads.each(&:join)
 
 
