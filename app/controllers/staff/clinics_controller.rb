8c21a6cf70064b9366f307b57e53dbd423bb1f9c
hiro <hironori.matsuse@gmail.com>
2019-12-10 18:55:38 +0900

    Add:画像の調整

--- app/controllers/staff/clinics_controller.rb
+++ app/controllers/staff/clinics_controller.rb
@@ -5,17 +5,16 @@ before_action :set_clinic, only: [:show, :edit, :update]
   end
 
   def edit
-    # @user = User.find(params[:id])
-    # @email = @user.email
+    @clinic = Clinic.find(params[:id])
   end
 
   def update
-    # @clinic = Clinic.find(params[:id])
-    # if @clinic.update(clinic_params)
-    #   redirect_to clinic_path, notice: '編集しました'
-    # else
-    #   render 'edit'
-    # end
+    @clinic = Clinic.find(params[:id])
+    if @clinic.update(clinic_params)
+      redirect_to clinic_path, notice: '編集しました'
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -28,7 +27,5 @@ before_action :set_clinic, only: [:show, :edit, :update]
     params.require(:clinic).permit(:name, :address, :phone_number, :image, :image_cache)
   end
 
-  def information_params
-    params.require(:information).permit(:comment)
-  end
+  
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,24 +1,21 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process :resize_to_limit => [50, 50]
-
   process :convert => 'jpg'
   
   version :thumb do
-    process :resize_to_limit => [300, 300]
+    process :resize_to_limit => [700, 700]
+  end
+
+  version :thumb50 do
+    process :resize_to_limit => [50, 50]
   end
 
   def extension_white_list
