0b74be7a7d44e1faae7269ff4d433ea12935f146
hiro <hironori.matsuse@gmail.com>
2019-12-24 23:36:22 +0900

    Fix:予約機能の修正

--- app/controllers/client/reservations_controller.rb
+++ app/controllers/client/reservations_controller.rb
@@ -12,29 +12,16 @@ class Client::ReservationsController < ApplicationController
     @reservations = Reservation.all
     respond_to do |format|
       format.html # => 通常のURLの場合、index.html.erb が返される
-      format.json { render json: @re } # URLが.jsonの場合、@products.to_json が返される
+      format.json { render json: @reservations } # URLが.jsonの場合、@products.to_json が返される
     end
   end
 
   def create
     @clinic = Clinic.find(params[:clinic_id])
-    # @reservations = Reservation.all.includes(:clinic)
     @reservation = @clinic.reservations.build(reservation_params)
     @reservation.patient = current_user.patient
-    @date_reservation = []
 
-    Reservation.all.each do |reservation|
-      @date_reservation.push([*reservation.start.to_i..reservation.end.to_i])
-    end
-
-    result = []
-   
-    binding.pry
-    @date_reservation.each do |reservation|
-      result.push(reservation & [*@reservation.start.to_i..@reservation.end.to_i])  
-    end 
-
-    if result.flatten.empty?
+    if @reservation.double_booking?
        @reservation.save
        redirect_to client_clinic_path(@clinic), notice: '作成しました'
     else
@@ -52,29 +39,22 @@ class Client::ReservationsController < ApplicationController
 
   def update
     @reservation = Reservation.find(params[:id])
-    respond_to do |format|
-      if @reservation.update(reservation_params)
-        format.html { redirect_to client_clinic_path(@clinic), notice: '編集しました' }
-        format.json { render :index }
+      if @reservation.double_booking?
+        @reservation.update(reservation_params)
+        redirect_to client_clinic_path(@clinic), notice: '編集しました'
       else
-        format.html { redirect_to client_clinic_path(@clinic), notice: '編集できませんでした。.' }
-        format.json { render :index }
+        redirect_to client_clinic_path(@clinic), notice: 'すでに予約が入っています。編集できませんでした。'
       end
-    end
   end
 
 
   def destroy
     @reservation = Reservation.find(params[:id])
-    respond_to do |format|
       if @reservation.destroy
-        format.html { redirect_to client_clinic_path(@clinic), notice: '削除しました.' }
-        format.json { render :index }
+        redirect_to client_clinic_path(@clinic), notice: '削除しました.'
       else
-        format.html { redirect_to client_clinic_path(@clinic), notice: '削除できませんでした。.' }
-        format.json { render :index }
+        redirect_to client_clinic_path(@clinic), notice: '削除できませんでした。' 
       end
-    end
   end
 
   private
