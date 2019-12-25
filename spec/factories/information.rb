19eaec1518f5e4e6f7c724114290033a4c2a56e0
hiro <hironori.matsuse@gmail.com>
2019-12-25 15:19:33 +0900

    Fix:マイグレーションファイルの修正

--- spec/factories/information.rb
+++ spec/factories/information.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :information do
-    
+    comment { '本日は休診です' }
+    clinic_id { 1 }
   end
 end
--- spec/models/information_spec.rb
+++ spec/models/information_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Information, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "intormationが空ならバリデーションが通らない" do
+    information = Information.new(comment: '')
+    expect(information).not_to be_valid
+  end
+
+  it "内容が記載されていればバリデーションが通る" do
+    information = FactoryBot.create(:information)
+    information = Information.create( comment: "本日は休診です",　clinic_id: "1")  
+    expect(information.errors).to_not include("を入力してください")
+    end
 end
