0c1b806571251d3b577a58e08c3501ff09fab4ff
hiro <hironori.matsuse@gmail.com>
2019-12-27 18:23:42 +0900

    del: rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'informations/index'
   get 'informations/new'
   get 'clinics/index'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
